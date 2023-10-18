e8d5764b85c62b0232a481996f3b814ef64b043d
Leo Basov <leo.basov@dlr.de>
2023-10-18 17:18:48 +0200

    refactored particles file

--- src/particles.jl
+++ src/particles.jl
@@ -1,31 +1,33 @@
 begin
 
-mutable struct Particle
-    pos
-    vel
-    spec
-    Particle() = new(zeros(3), zeros(3), -one(Int))
-end
+    import PhysicalConstants.CODATA2018: k_B as kb
 
-mutable struct Species
-    mass
-    Species() = new(1.0)
-end
-
-mutable struct Particles
-    particles::Array{Particle}
-    species::Array{Species}
-    Particles() = new(Array{Particle}(undef, 0), Array{Species}(undef, 0))
-end
-
-function create_particles(species, T, N)
-    particles = Array{Particle}(undef, N)
-
-    for i = 1:N
-        particles[i] = Particle()
+    mutable struct Particle
+        pos
+        vel
+        spec
+        Particle() = new(zeros(3), zeros(3), -one(Int))
     end
 
-    return particles
-end
+    mutable struct Species
+        mass
+        Species() = new(1.0)
+    end
+
+    mutable struct Particles
+        particles::Array{Particle}
+        species::Array{Species}
+        Particles() = new(Array{Particle}(undef, 0), Array{Species}(undef, 0))
+    end
+
+    function create_particles(species, T, N)
+        particles = Array{Particle}(undef, N)
+
+        for i = 1:N
+            particles[i] = Particle()
+        end
+
+        return particles
+    end
 
 end
